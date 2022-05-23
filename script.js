41d9a86517e3cd3e1bfa3047ac341b812affb2f8
선종헌 <frontseon@gmail.com>
2022-05-23 13:45:10 +0900

    #0002 Swiper

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-$(function(){
-    $('.test').on('click', function(){
+$(function () {
+    $('.test').on('click', function () {
         $(this).toggleClass('active');
+    });
+    // swiper
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'vertical',
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
     });
 });
