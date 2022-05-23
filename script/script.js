591e9649a23a40d00ca23bc5182242c1bd90a7a4
선종헌 <frontseon@gmail.com>
2022-05-23 13:58:21 +0900

    #0002 Directory Test

--- script/script.js
+++ script/script.js
@@ -2,24 +2,18 @@ $(function () {
     $('.test').on('click', function () {
         $(this).toggleClass('active');
     });
+    
     // swiper
     const swiper = new Swiper('.swiper', {
-        // Optional parameters
         direction: 'vertical',
-        loop: true,
-
-        // If we need pagination
+        loop: false,
         pagination: {
             el: '.swiper-pagination',
         },
-
-        // Navigation arrows
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-
-        // And if we need scrollbar
         scrollbar: {
             el: '.swiper-scrollbar',
         },
