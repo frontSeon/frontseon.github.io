37006ef5bdb6cd0d68411ff2a9b1822481a4aa70
선종헌 <frontseon@gmail.com>
2022-05-30 16:22:26 +0900

    #0008 Swiper & Button

--- script/script.js
+++ script/script.js
@@ -5,35 +5,48 @@ $(function () {
         direction: 'vertical',
         resistanceRatio: 0,
         loop: false,
+        mousewheel: true,
+        keyboard: true,
+        
+        // navigation-button
+        on: {
+            slideChangeTransitionStart: function () {
+                var n = this.activeIndex;
+                changeButton(n);
+            }
+        },
         pagination: {
             el: '.swiper-pagination',
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
+        }
     });
 
-    // logo
-    $('.navi_logo').click(function() {
-        location.reload();
-    });
-
+    // navigation-button
+    var buttonItem = $('.navi_menu-mobile__btn');
+    buttonItem.click(function () {
+        var ind = $(this).index();
+        changeButton(ind);
+        swiper.slideTo(ind);
+    })
+    function changeButton(index) {
+        buttonItem.removeClass('active').eq(index).addClass('active');
+    }
 
     // navigation-mobile
-    $('.navi_menu__btn').on('click', function(){
+    $('.navi_menu__btn').on('click', function () {
         $('.navi_menu-mobile__btn').toggleClass('hidden');
     });
-    $('.navi_menu__btn').on('click', function(){
+    $('.navi_menu__btn').on('click', function () {
         $('.navi_menu__btn_active').toggleClass('hidden');
     });
-    $('.section').on('click', function(){
-        if(!$('.navi_menu-mobile__btn').hasClass('hidden')){
+    $('.section').on('click', function () {
+        if (!$('.navi_menu-mobile__btn').hasClass('hidden')) {
             $('.navi_menu-mobile__btn').addClass('hidden')
             $('.navi_menu__btn_active').addClass('hidden')
         }
-    })
+    });
+
+    // logo
+    $('.navi_logo').click(function () {
+        location.reload();
+    });
 });
