02026d0aa8b7869c0c65f999fa8b1ce84b1a2d83
선종헌 <frontseon@gmail.com>
2022-06-08 13:07:38 +0900

    #0016 Edit Lottie

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,10 @@
 // Modules 호출
 var gulp = require('gulp');
 concat = require('gulp-concat'),
-// 분리된 파일 합치는거
-
 cleanCSS = require('gulp-clean-css'),
 rename = require('gulp-rename'),
 sass = require('gulp-sass')(require('node-sass')),
 sourcemaps = require('gulp-sourcemaps'),
-// 개발자코드에서 최종 CSS 말고 원래 파일위치 찍히게 해주는거
 
 gulp.task('all', () => {
     return (
